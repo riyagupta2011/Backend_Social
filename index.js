1ecac1d39f531f60219efdb0ce8da02d47291b4f
Riya Gupta <98754072+riyagupta2011@users.noreply.github.com>
2023-08-17 15:47:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const path=require("path")
 const app=express();
 
 dotenv.config();
-
+app.use(CORS())
 mongoose.connect(process.env.MONGO_URL,{useNewUrlParser:true}).then(()=>{
     console.log("Mongo db connected")
 }).catch((err)=>console.log(err));
@@ -33,6 +33,7 @@ const storage=multer.diskStorage({
 const upload=multer({storage: storage});
 app.post("/api/upload",upload.single("file"),(req,res)=>{
    try{
+       res.setHeader("Access-Control-Allow-Credentials","true");
       return res.status(200).json("File uploaded successfully")
    } 
    catch(err)
@@ -44,6 +45,6 @@ app.post("/api/upload",upload.single("file"),(req,res)=>{
 app.use("/api/users",userRoute);
 app.use("/api/auth",authRoute);
 app.use("/api/post",postRoute);
-app.listen(8800,()=>{
+app.listen(PORT,()=>{
     console.log("backend server is running")
 })
